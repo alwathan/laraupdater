5e9c84d23eac8a4b46b34f4c200cbff7f861f35a
alwathan <jazuli.gh@gmail.com>
2020-07-10 23:46:05 +0700

    commit

--- src/LaraUpdaterController.php
+++ src/LaraUpdaterController.php
@@ -26,7 +26,15 @@ class LaraUpdaterController extends Controller
         //var_dump($versions);
 
         $latest_version = $versions['tag_name'];
-        $current_version = File::get(public_path('version.txt'));
+        
+        if(Storage::disk('local')->missing('version.txt')){
+            if(Storage::disk('local')->put('version.txt',  config('laraupdater.app_version'))){
+                $current_version = Storage::disk('local')->get('version.txt');
+            }
+        }else{
+            $current_version = Storage::disk('local')->get('version.txt');
+        }
+
         if($current_version < $latest_version){
             echo "Tersedia versi terbaru: ".$latest_version;
         }else{
@@ -66,11 +74,57 @@ class LaraUpdaterController extends Controller
     }
 
     public function test(){
-        if(Storage::disk('local')->missing('vesion.txt')){
-            echo config('laraupdater.app_version');
-            Storage::disk('local')->put('version.txt',  config('laraupdater.app_version'));
+        $token = config('laraupdater.github.personal_access_token');
+        $vendor = config('laraupdater.github.vendor');
+        $repo = config('laraupdater.github.repository');
+
+    	$data = Http::withToken($token)->get('https://api.github.com/repos/'.$vendor.'/'.$repo.'/releases/latest');
+        
+        $versions = $data->json();
+
+        if($data->status() != '200'){
+            echo "Persoanl access token invalid!";
         }else{
-            echo Storage::disk('local')->get('version.txt');
+            $latest_version = $versions['tag_name'];
+            
+            if(Storage::disk('local')->missing('version.txt')){
+                if(Storage::disk('local')->put('version.txt',  config('laraupdater.app_version'))){
+                    $current_version = Storage::disk('local')->get('version.txt');
+                }
+            }else{
+                $current_version = Storage::disk('local')->get('version.txt');
+            }
+
+            if($current_version < $latest_version){
+                $zzip = Http::withToken($token)->get($versions['zipball_url']);
+                $zipname = $latest_version.".zip";
+
+                if(Storage::disk('local')->put($zipname, $zzip->body())){
+                    $ziplocation = storage_path('app/'.$zipname);
+                    $zip = new ZipArchive();
+                    if($zip->open($ziplocation)){
+                        if($zip->extractTo(storage_path('app'))){
+                            $zip->close();
+
+                            $directories = Storage::disk('local')->directories();
+                            $output = preg_grep('!^alwathan-pilbup!', $directories);
+                            
+                            if(File::copyDirectory(storage_path('app')."/".$output[0], base_path())){
+                                Storage::disk('local')->put('version.txt',$latest_version);
+                                Storage::disk('local')->deleteDirectory($output[0]);
+                                Storage::disk('local')->delete($zipname);
+                                echo "berhasil melakukan update";
+                            }
+                        }
+                    }else{
+                        echo "Gagal extract zip";
+                    }
+                }else{
+                    echo "Gagal download zip";
+                }
+            }else{
+                echo "Tidak ada versi terbaru";
+            }
         }
     }
 }
